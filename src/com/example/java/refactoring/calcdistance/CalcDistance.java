10a30daafeccbc45e8ee88e65b1a4be8910a74b8
kwanghoon <lazyswamp@gmail.com>
2018-05-27 16:59:31 +0900

    Initial commit.

---
+++ src/com/example/java/refactoring/calcdistance/CalcDistance.java
@@ -0,0 +1,23 @@
+package com.example.java.refactoring.calcdistance;
+
+import java.util.Scanner;
+
+public class CalcDistance {
+
+	public static void main(String[] args) {
+		int x, y;
+		Scanner scan = new Scanner(System.in);
+		System.out.print("첫 번째 점의 좌표 x와 y를 입력하세요.");
+		x = scan.nextInt();
+		y = scan.nextInt();
+		Point p = new Point(x, y);
+		System.out.println("두 번째 점의 좌표 x와 y를 입력하세요.");
+		x = scan.nextInt();
+		y = scan.nextInt();
+		Point q = new Point(x,y);
+		System.out.printf("%s과(와) %s 사이의 거리 = %5.2f",
+				p.toString(), q.toString(),
+				p.distanceFrom(q));
+	}
+
+}
