1d8b19ad125fb5388898e423615cd016b9abe3b2
Kwanghoon Choi <lazyswamp@gmail.com>
2022-05-25 08:58:32 +0900

    add two invoice examples and their test cases

---
+++ src/com/example/refactoring/invoice/Order.java
@@ -0,0 +1,14 @@
+package com.example.refactoring.invoice;
+
+public class Order {
+	private double _amount;
+	
+	public Order(double _amount) {
+		super();
+		this._amount = _amount;
+	}
+
+	public double getAmount() {
+		return _amount;
+	}
+}
---
+++ src/com/example/refactoring/invoice/Printer.java
@@ -0,0 +1,5 @@
+package com.example.refactoring.invoice;
+
+public interface Printer {
+	void print(String msg);
+}
